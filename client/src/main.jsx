2693af5e7d2af28e37fd77f75401b7e0c0d09850
Your Name <gastonnietoarte@gmail.com>
2023-05-15 02:55:15 -0300

    nuevo domain

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -9,11 +9,11 @@ import store from "./redux/store/index.js";
 import { Auth0Provider } from "@auth0/auth0-react";
 import ScrollToTop from "./components/ScrollToTop/ScrollToTop";
 import axios from 'axios';
+import { HashRouter } from "react-router-dom"; // M
 
 const domain =import.meta.env.VITE_AUTH0_DOMAIN;
 const clientId =import.meta.env.VITE_AUTHO_CLIENT_ID;
 
-import { HashRouter } from "react-router-dom"; // M
 
 
 /* axios.defaults.baseURL="http://localhost:3001"; */
@@ -21,8 +21,14 @@ axios.defaults.baseURL="https://poke-back.up.railway.app/";
 
 console.log(domain, clientId);
 
+const handleRedirectCallback = (appState) => {
+  // redireccionar al usuario a una URL predeterminada
+  history.push(appState?.returnTo || '/create');
+};
+
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Auth0Provider domain={domain} clientId={clientId} redirectUri="https://g4s70n.github.io/poke/callback" >
+  <Auth0Provider domain={domain} clientId={clientId} redirectUri="https://g4s70n.github.io/poke"   onRedirectCallback={handleRedirectCallback}>
     <Provider store={store}>
       <HashRouter>
         <ScrollToTop />
